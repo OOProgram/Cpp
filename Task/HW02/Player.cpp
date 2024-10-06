e202f47258b85107af2ff5e2b72092f0439e836e
KTNote <jdongel2004@naver.com>
2024-10-06 23:55:00 +0900

    upload HW02

---
+++ Task/HW02/Player.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Player.h"
+using namespace std;
+
+Player::Player() {
+	name = "";
+}
+
+string Player::getName() {
+	return name;
+}
+
+void Player::setName(string name) {
+	this->name = name;
+}
---
+++ Task/HW02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#include "GamblingGame.h", "Player.h"
+
+int main() {
+	GamblingGame game;
+	game.play();
+	return 0;
+}
