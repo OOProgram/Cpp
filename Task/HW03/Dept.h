b2c2ccdaaf586fdcec3e4c82b2ae3a8d9563806e
KTNote <jdongel2004@naver.com>
2024-10-15 23:04:44 +0900

    F

---
+++ Task/HW03/Dept.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class Dept {
+	int size;   //scores 배열의 크기
+	int* scores;   //동적 할당 받을 정수 배열의 주소
+public:
+	Dept(int size);   //생성자
+	Dept(const Dept& dept);   //복사 생성자
+	~Dept();   //소멸자
+	int getSize();
+	void read();   //size만큼 키보드에서 정수를 읽어 scores 배열에 저장
+	bool isOver60(int index);
+};
+
+int countPass(Dept dept);
---
+++ Task/HW03/Dept1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Dept1.h"
+using namespace std;
+
+Dept::Dept(int size) {
+	this->size = size;
+	scores = new int[size];
+}
+
+Dept::~Dept() {
+	if (scores)
+		delete[] scores;
+}
+
+int Dept::getSize() {
+	return size;
+}
+
+void Dept::read() {
+	cout << size << "개 점수 입력>>";
+	for (int i = 0; i < size; i++)
+		cin >> scores[i];
+}
+
+bool Dept::isOver60(int index) {
+	return scores[index] > 60;
+}
+
+int countPass(Dept& dept) {
+	int count = 0;
+	for (int i = 0; i < dept.getSize(); i++) {
+		if (dept.isOver60(i))
+			count++;
+	}
+	return count;
+}
