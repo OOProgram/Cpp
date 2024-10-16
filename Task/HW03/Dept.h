f13de2a2c1dc3a4e5a1d70613cb500df41a6cedc
darly <yumdarly4370@gmail.com>
2024-10-16 14:50:25 +0900

    .

--- Task/HW03/Dept.h
+++ Task/HW03/Dept.h
@@ -2,14 +2,14 @@
 #include <iostream>
 
 class Dept {
-	int size;   //scores 배열의 크기
-	int* scores;   //동적 할당 받을 정수 배열의 주소
+	int size;
+	int* scores;
 public:
-	Dept(int size);   //생성자
-	Dept(const Dept& dept);   //복사 생성자
-	~Dept();   //소멸자
+	Dept(int size);
+	Dept(const Dept& dept); 
+	~Dept();
 	int getSize();
-	void read();   //size만큼 키보드에서 정수를 읽어 scores 배열에 저장
+	void read();
 	bool isOver60(int index);
 };
 
